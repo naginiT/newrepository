9223008f6a8505b598f2b4ba9382893a0ba518de
sada <jon7sada@gmail.com>
2018-07-10 15:59:22 +0530

    commit

---
+++ KeyWordDrivenPro/src/BrowserInvoking/Browser.java
@@ -0,0 +1,33 @@
+package BrowserInvoking;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Browser
+{
+	public static WebDriver driver;
+	public static void brosercode()
+	{
+		System.setProperty("webdriver.chrome.driver", "H:\\app selenium\\chromedriver.exe");
+		 driver=new ChromeDriver();
+	}
+	public static void fbappInvoke()
+	{
+		
+		driver.manage().window().maximize();
+		driver.get("https://www.facebook.com");
+	}
+	public static void fbemail()
+	{
+		
+		driver.findElement(By.xpath("//*[@id='email']")).sendKeys("pyatarojareddy@gmail.com");
+		
+	}
+	public static void fbpass()
+	{
+		
+	driver.findElement(By.xpath("//*[@id='pass']")).sendKeys("1424235");
+		
+	}
+}
